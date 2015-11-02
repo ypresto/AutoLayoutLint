1c73b07baf3169f246c63e3259a076e4529f9f6c
Yuya Tanaka <yuya.presto@gmail.com>
2015-11-02 12:22:37 +0900

    Check-in Pods direcotry for Carthage compatibility

---
+++ Example/Pods/Target Support Files/AutoLayoutLint/AutoLayoutLint-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AutoLayoutLint : NSObject
+@end
+@implementation PodsDummy_AutoLayoutLint
+@end
---
+++ Example/Pods/Target Support Files/AutoLayoutLint/AutoLayoutLint-umbrella.h
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+
+#import "PSTAutoLayoutLintTestCase.h"
+#import "PSTAutoLayoutMethodSwizzling.h"
+#import "PSTViewControllerFinder.h"
+#import "PSTViewControllerMethodSwizzling.h"
+
+FOUNDATION_EXPORT double AutoLayoutLintVersionNumber;
+FOUNDATION_EXPORT const unsigned char AutoLayoutLintVersionString[];
+
